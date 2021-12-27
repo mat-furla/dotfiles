8adee54ceff6183660e4e5b1fa2d9f270c463be6
Matheus Furlanetto <matheus.furlanetto@la.logicalis.com>
2021-12-27 20:59:46 -0300

    feat: updated colors to gruvbox

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,7 @@
 ## ABREVIATIONS
 abbr sd 'sudo'
 abbr ls 'n'
+abbr o 'micro'
 abbr N 'sudo -E nnn'
 abbr dl 'cd $HOME/Downloads'
 abbr doc 'cd $HOME/Documents'
@@ -18,7 +19,6 @@ abbr u 'sudo xbps-install -Syyu'
 abbr r 'sudo xbps-remove -Rcn'
 abbr c 'sudo xbps-remove -o'
 
-abbr code 'code-oss'
 abbr gs 'git status'
 abbr ga 'git add --all'
 abbr gl 'git pull'
@@ -45,18 +45,60 @@ set -gx _JAVA_AWT_WM_NONREPARENTING 1
 
 set -gx BROWSER firefox
 set -gx VISUAL leafpad
-set -gx EDITOR nano
+set -gx EDITOR micro
 
+## SYNTAX HIGHLIGHT
+set color00 1d/20/21 # Base 00 - Black
+set color01 fb/49/34 # Base 08 - Red
+set color02 b8/bb/26 # Base 0B - Green
+set color03 fa/bd/2f # Base 0A - Yellow
+set color04 83/a5/98 # Base 0D - Blue
+set color05 d3/86/9b # Base 0E - Magenta
+set color06 8e/c0/7c # Base 0C - Cyan
+set color07 d5/c4/a1 # Base 05 - White
+set color08 66/5c/54 # Base 03 - Bright Black
+set color09 $color01 # Base 08 - Bright Red
+set color10 $color02 # Base 0B - Bright Green
+set color11 $color03 # Base 0A - Bright Yellow
+set color12 $color04 # Base 0D - Bright Blue
+set color13 $color05 # Base 0E - Bright Magenta
+set color14 $color06 # Base 0C - Bright Cyan
+set color15 fb/f1/c7 # Base 07 - Bright White
+set color16 fe/80/19 # Base 09
+set color17 d6/5d/0e # Base 0F
+set color18 3c/38/36 # Base 01
+set color19 50/49/45 # Base 02
+set color20 bd/ae/93 # Base 04
+set color21 eb/db/b2 # Base 06
+set colorfg $color07 # Base 05 - White
+set colorbg $color00 # Base 00 - Black
 
-## FLATPAK
-set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
-set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
-
-for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
-    if test -d $flatpakdir
-        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
-    end
-end
+set -U fish_color_autosuggestion 504945
+set -U fish_color_cancel -r
+set -U fish_color_command green #white
+set -U fish_color_comment 504945
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end brblack #blue
+set -U fish_color_error red
+set -U fish_color_escape yellow #green
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_match --background=brblue
+set -U fish_color_normal normal
+set -U fish_color_operator blue #green
+set -U fish_color_param bdae93
+set -U fish_color_quote yellow #brblack
+set -U fish_color_redirection cyan
+set -U fish_color_search_match bryellow --background=504945
+set -U fish_color_selection white --bold --background=504945
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description yellow --dim
+set -U fish_pager_color_prefix white --bold #--underline
+set -U fish_pager_color_progress brwhite --background=cyan
 
 
 ## PROMPT
